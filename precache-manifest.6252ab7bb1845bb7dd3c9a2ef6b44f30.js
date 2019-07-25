ebf983ba61bfdb812c85f58281c909f0917c0ccc
mebird <mbe_pro@outlook.com>
2019-07-24 20:49:04 -0700

    Updates

---
+++ precache-manifest.6252ab7bb1845bb7dd3c9a2ef6b44f30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e58fd6eaf8fa055cadeb1446af7ecab",
+    "url": "/basic-counter/index.html"
+  },
+  {
+    "revision": "a40ff139853683386975",
+    "url": "/basic-counter/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "c19ba6ddb55bb7883655",
+    "url": "/basic-counter/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a40ff139853683386975",
+    "url": "/basic-counter/static/js/2.1de66709.chunk.js"
+  },
+  {
+    "revision": "c19ba6ddb55bb7883655",
+    "url": "/basic-counter/static/js/main.7651539d.chunk.js"
+  },
+  {
+    "revision": "c571ef56e2bec22160c5",
+    "url": "/basic-counter/static/js/runtime~main.d369a8ea.js"
+  }
+]);
